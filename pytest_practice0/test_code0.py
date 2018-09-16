f6a91952e9e218720720bc59a45e8a07e221f3bc
watabomb <watabomb@gmail.com>
2018-09-17 00:23:46 +0900

    ・pytest -m で評価されるマークのテスト ・例外が発生するだけで失敗とみなされる件の確認

--- pytest_practice0/test_code0.py
+++ pytest_practice0/test_code0.py
@@ -1,6 +1,17 @@
 # coding: utf-8
+import pytest
+
+# pytest -m run_mark0 と指定すると、以下のマークがされた関数のみ評価される
+@pytest.mark.run_mark0
 def test_func0():
     assert (1,2,3) == (1,2,3)
+
+# assert書かなくても例外が発生すると失敗とみなされる
+def test_func1():
+    x=1
+    y=0
+    z=x/y
+    return
 
 # テスト対象外(pytest.ini を参照)
 def notTest_func0():
